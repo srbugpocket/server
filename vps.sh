32b75db2861ee6fca51e4d1468a802bcaa3c920d
srbugpocket <srbugpocket@gmail.com>
2026-02-04 17:30:40 -0300

    Change Ubuntu 22.04 image link to live server ISO
    Updated the download link for Ubuntu 22.04 to point to the live server ISO.

--- vps.sh
+++ vps.sh
@@ -228,7 +228,7 @@ mkdir -p "$VM_DIR"
 
 declare -A OS_OPTIONS=(
     ["Ubuntu 20.04"]="ubuntu|fossa|https://cloud-images.ubuntu.com/focal/current/focal-server-cloudimg-amd64.img|ubuntu20|ubuntu|ubuntu"
-    ["Ubuntu 22.04"]="ubuntu|jammy|https://cloud-images.ubuntu.com/jammy/current/jammy-server-cloudimg-amd64.img|ubuntu22|ubuntu|ubuntu"
+    ["Ubuntu 22.04"]="ubuntu|jammy|https://releases.ubuntu.com/jammy/ubuntu-22.04.5-live-server-amd64.iso|ubuntu22|ubuntu|ubuntu"
     ["Ubuntu 24.04"]="ubuntu|noble|https://cloud-images.ubuntu.com/noble/current/noble-server-cloudimg-amd64.img|ubuntu24|ubuntu|ubuntu"
     ["Debian 11"]="debian|bullseye|https://cloud.debian.org/images/cloud/bullseye/latest/debian-11-generic-amd64.qcow2|debian11|debian|debian"
     ["Debian 12"]="debian|bookworm|https://cloud.debian.org/images/cloud/bookworm/latest/debian-12-generic-amd64.qcow2|debian12|debian|debian"
