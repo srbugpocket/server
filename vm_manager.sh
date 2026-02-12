b086d33ffd2ff87d52be3f8088bddb777a7d1223
srbugpocket <srbugpocket@gmail.com>
2026-02-12 09:51:39 -0300

    Update vm_manager.sh

--- vm_manager.sh
+++ vm_manager.sh
@@ -336,8 +336,7 @@ start_vm() {
             -cpu max
             -drive file=$IMG_FILE,format=raw,if=virtio
             -drive "file=$SEED_FILE,format=raw,if=virtio"
-            -cdrom debian-live.iso
-            -boot d
+            -boot order=3
             -device virtio-net-pci,netdev=n0
             -nographic
             -netdev "user,id=n0,hostfwd=tcp::$SSH_PORT-:22"
