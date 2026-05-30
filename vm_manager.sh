563e8e84e5f67d156ec5e3482289b1e59b12c0bf
srbugpocket <srbugpocket@gmail.com>
2026-05-29 21:20:09 -0300

    Update vm_manager.sh

--- vm_manager.sh
+++ vm_manager.sh
@@ -366,9 +366,7 @@ start_vm() {
             -cpu max
             -drive file=$IMG_FILE,format=raw,if=virtio
             -drive "file=$SEED_FILE,format=raw,if=virtio"
-            -device virtio-net-pci,netdev=n0
             -nographic
-            -netdev "user,id=n0,hostfwd=tcp::$SSH_PORT-:22"
         )
 
         # Add port forwards if specified
